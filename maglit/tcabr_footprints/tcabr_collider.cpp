dd98f3f918c0fc1a528644f8239ad6b8423ad435
José Fernandes <jose.r.fernandes.j@gmail.com>
2024-02-12 22:32:01 -0300

    added the maglit/tcabr_footprints directory

---
+++ maglit/tcabr_footprints/tcabr_collider.cpp
@@ -0,0 +1,97 @@
+typedef struct {
+    int     np;  // number of nodes
+    double *R;   // node coords
+    double *Z;   //
+    double  Rc;  // center coords
+    double  Zc;  //
+    double *th;  // sector angles
+    int     idx; // current index
+} tcabr_shape;
+
+bool load_shape(const char path[], tcabr_shape *shape) {
+    FILE *f0 = fopen(path, "r");
+    if (!f0) {
+        printf("load_shape: %s not found\n", path);
+        return false;
+    } else {
+        printf("load_shape: %s loaded\n", path);
+    }
+    fscanf(f0, "%d", &(shape->np));
+    shape->R = new double[shape->np];
+    shape->Z = new double[shape->np];
+    shape->th = new double[shape->np + 1];
+    double Rmin, Rmax, Zmin, Zmax;
+    for (int i = 0; i < (shape->np); i++) {
+        fscanf(f0, "%lf", shape->R + i);
+        fscanf(f0, "%lf", shape->Z + i);
+        if (shape->R[i] < Rmin || i == 0)
+            Rmin = shape->R[i];
+        if (shape->R[i] > Rmax || i == 0)
+            Rmax = shape->R[i];
+        if (shape->Z[i] < Zmin || i == 0)
+            Zmin = shape->Z[i];
+        if (shape->Z[i] > Zmax || i == 0)
+            Zmax = shape->Z[i];
+    }
+    shape->Rc = 0.5 * (Rmin + Rmax);
+    shape->Zc = 0.5 * (Zmin + Zmax);
+    printf("shape Rc, Zc = %f, %f\n", shape->Rc, shape->Zc);
+    for (int i = 0; i < (shape->np); i++) {
+        shape->th[i] = atan2(shape->Z[i] - shape->Zc, shape->R[i] - shape->Rc);
+    }
+    shape->th[shape->np] = shape->th[0] + 2 * M_PI;
+    fclose(f0);
+    return true;
+}
+
+void free_shape(tcabr_shape *shape) {
+    delete[] shape->R;
+    delete[] shape->Z;
+    delete[] shape->th;
+    delete shape;
+}
+
+bool search_index(double ang, tcabr_shape *shape) {
+    // printf("search_index: theta[%d] = %f, ang = %f\n", shape->idx, shape->th[shape->idx], ang);
+    if (ang >= shape->th[shape->idx] && ang < shape->th[shape->idx + 1]) {
+        return true;
+    } else if (ang < shape->th[shape->idx]) {
+        if (shape->idx == 0)
+            shape->idx = shape->np - 1;
+        else
+            shape->idx--;
+        return search_index(ang, shape);
+    } else if (ang > shape->th[shape->idx + 1]) {
+        if (shape->idx == shape->np - 1)
+            shape->idx = 0;
+        else
+            shape->idx++;
+        return search_index(ang, shape);
+    } else {
+        printf("search_index: undefined behavior\n");
+        return false;
+    }
+}
+
+// calculate AB x AC
+double
+cross(double RA, double ZA, double RB, double ZB, double RC, double ZC) {
+    double dR1 = RB - RA;
+    double dZ1 = ZB - ZA;
+    double dR2 = RC - RA;
+    double dZ2 = ZC - ZA;
+    return dR1 * dZ2 - dR2 * dZ1;
+}
+
+bool tcabr_inside(double R, double Z, double phi, void *aux) {
+    tcabr_shape *shape = (tcabr_shape *)aux;
+    double       ang = atan2(Z - shape->Zc, R - shape->Rc);
+    if (!search_index(ang, shape))
+        return false;
+
+    double axb = cross(shape->R[shape->idx],
+                       shape->Z[shape->idx],
+                       shape->R[shape->idx + 1],
+                       shape->Z[shape->idx + 1], R, Z);
+    return (axb >= 0);
+}
---
+++ maglit/tcabr_footprints/test.cpp
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "../../sode/sode.cpp"
+#include "../auxfields.cpp"
+#include "../maglit.cpp"
+#include "tcabr_collider.cpp"
+
+//  COMPILATION: make test
+
+// function to map coordinates in vessel and get scalar values
+typedef struct
+{
+    double deltaPhi;
+    double length;
+    double psimin;
+} map_scalars;
+
+void   map_wall(maglit &tracer, auxfields &aux_field, double R0, double Z0, double phi0, double &R1, double &Z1, double &phi1, map_scalars &scalars);
+void   status_printer(int status);
+double dist(double R0, double Z0, double phi0, double R1, double Z1, double phi1);
+
+int main() {
+    char   source_path[] = "../our_m3dc1_data/i_coils/n03/C1.h5";
+    char   shape_path[] = "tcabr_first_wall_m3dc1";
+    char   output_path[] = "../orbits/i_coils_n3_02.dat";
+    double Rmin = 0.470;
+    double Rmax = 0.487;
+    int    nR = 300;
+    int    nPhi = 300;
+
+    Check if the output file name is unique if (access(output_path, F_OK) != -1) {
+        printf("There is a file with the same name saved to the chosen directory. Please change the output file name to avoid overwriting your data. \n");
+        return 1;
+    }
+
+    tcabr_shape *shape = new tcabr_shape;
+    if (!load_shape(shape_path, shape))
+        return -1;
+
+    maglit    tracer(source_path, FIO_M3DC1_SOURCE);
+    auxfields aux_field(source_path, FIO_M3DC1_SOURCE);
+    tracer.set_inside(shape, tcabr_inside);
+    double dphi_init = 0.01;
+    double dphi_min = 1e-5;
+    double dphi_max = 0.2;
+    tracer.configure(dphi_init, dphi_min, dphi_max);
+    // tracer.set_verb(); // activate messages
+
+    double Zfloor = -0.24;
+
+    map_scalars scalars;
+    FILE       *f0 = fopen(output_path, "w");
+    printf("Running wall grid\n");
+    fprintf(f0, "#R0 Z0 phi0 R1 Z1 phi1 deltaPhi length psiMin\n");
+    for (int i = 0; i < nPhi / 3; i++) {
+        double Z0 = Zfloor;
+        double Z1;
+        double phi0 = 2 * M_PI * i / nPhi;
+        double phi1;
+        for (int j = 0; j < nR; j++) {
+            double R0 = Rmin + (Rmax - Rmin) * j / nR;
+            double R1;
+            map_wall(tracer, aux_field, R0, Z0, phi0, R1, Z1, phi1, scalars);
+            fprintf(f0, "%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f\n", R0, Z0, phi0, R1, Z1, phi1, scalars.deltaPhi, scalars.length, scalars.psimin);
+            printf("%f%%\n", 100.0 * (i * nR + j) / (nR * nPhi));
+        }
+    }
+
+    fclose(f0);
+    free_shape(shape);
+    return 0;
+}
+
+// test function for psi (obsolete - NOT BEING USED)
+void calc_fields(double R, double Z, double phi) {
+    double      psi[1];
+    double      x2[3] = {R, phi, Z};
+    const char *source_path = "../our_m3dc1_data/i_coils/n03/C1.h5";
+
+    fio_source     *src2;
+    fio_field      *psi_field;
+    fio_option_list opt2;
+
+    fio_open_source(&src2, FIO_M3DC1_SOURCE, source_path);
+
+    src2->get_field_options(&opt2);
+    opt2.set_option(FIO_TIMESLICE, 0);
+    opt2.set_option(FIO_PART, FIO_TOTAL);
+
+    src2->get_field(FIO_POLOIDAL_FLUX_NORM, &psi_field, &opt2);
+
+    psi_field->eval(x2, psi);
+    printf("psi = %f\n", *psi);
+}
+
+void map_wall(maglit &tracer, auxfields &aux_field, double R0, double Z0, double phi0, double &R1, double &Z1, double &phi1, map_scalars &scalars) {
+    int    status;
+    double phi_max = 100 * 2 * M_PI;
+    R1 = R0;
+    Z1 = Z0;
+    phi1 = phi0;
+    tracer.reset();
+    double arc = 0;
+    double psin[1] = {5};
+    scalars.psimin = *psin;
+    do {
+        R0 = R1;
+        Z0 = Z1;
+        phi0 = phi1;
+        status = tracer.step(R1, Z1, phi1, phi_max, -1);
+        if (status == SODE_CONTINUE_GOOD_STEP) {
+            arc += dist(R0, Z0, phi0, R1, Z1, phi1);
+            aux_field.psin_eval(R1, phi1, Z1, psin);
+        }
+        if (*psin < scalars.psimin)
+            scalars.psimin = *psin;
+    } while (status == SODE_CONTINUE_GOOD_STEP || status == SODE_CONTINUE_BAD_STEP);
+    scalars.deltaPhi = phi1 - phi0;
+    scalars.length = arc;
+    printf("\n%f\n", scalars.psimin);
+    status_printer(status);
+}
+
+void status_printer(int status) {
+    switch (status) {
+    case SODE_OK:
+        printf("SODE_OK\n");
+        break;
+    case SODE_SUCCESS_TIME:
+        printf("SODE_SUCCESS_TIME\n");
+        break;
+    case SODE_SUCCESS_MONITOR:
+        printf("SODE_SUCCESS_MONITOR\n");
+        break;
+    case SODE_CONTINUE_GOOD_STEP:
+        printf("SODE_CONTINUE_GOOD_STEP\n");
+        break;
+    case SODE_CONTINUE_BAD_STEP:
+        printf("SODE_CONTINUE_BAD_STEP\n");
+        break;
+    case SODE_FAILED:
+        printf("SODE_FAILED\n");
+        break;
+    case SODE_BAD_FUNC:
+        printf("SODE_BAD_FUNC\n");
+        break;
+    default:
+        printf("Unknown Status\n");
+        break;
+    }
+}
+
+double
+dist(double R0, double Z0, double phi0, double R1, double Z1, double phi1) {
+    double dx = R1 * cos(phi1) - R0 * cos(phi0);
+    double dy = R1 * sin(phi1) - R0 * sin(phi0);
+    double dz = Z1 - Z0;
+    return sqrt(dx * dx + dy * dy + dz * dz);
+}
