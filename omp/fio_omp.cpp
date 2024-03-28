e5e81fad0e870504e3103680155df0f05f701ea9
ojrfernandes <jose.r.fernandes.j@gmail.com>
2024-03-28 17:51:16 -0300

    debadded maglib/omp ug configuration files

--- omp/fio_omp.cpp
+++ omp/fio_omp.cpp
@@ -1,58 +1,61 @@
 #include <fusion_io.h>
 #include <iostream>
 #include <omp.h>
-#include <sstream>
 
 int main() {
 
-    // Path to the source file
+    fio_source *src;
     const char *source_path = "/home/jfernandes/Software/maglib_local/maglit/our_m3dc1_data/i_coils/n03/C1.h5";
+
+    int result = fio_open_source(&src, FIO_M3DC1_SOURCE, source_path);
+    if (result != FIO_SUCCESS) {
+        std::cout << "Error opening file " << source_path << std::endl;
+        delete (src);
+        return -1;
+    }
+
+    fio_option_list opt;
+    (*src).get_field_options(&opt);
+    opt.set_option(FIO_TIMESLICE, 0);
+    opt.set_option(FIO_PART, FIO_TOTAL);
+
+    fio_field *mag_field;
+    result = (*src).get_field(FIO_MAGNETIC_FIELD, &mag_field, &opt);
+    if (result != FIO_SUCCESS) {
+        std::cout << "Error opening file " << source_path << std::endl;
+        mag_field = 0;
+        return -1;
+    }
 
     double B[10][3];
     double x[10][3];
+    int    h_array[10];
 
-#pragma omp parallel
-    {
-        fio_source *src;
-        int         result = fio_open_source(&src, FIO_M3DC1_SOURCE, source_path);
-        if (result != FIO_SUCCESS) {
-            std::cout << "Error opening file " << source_path << std::endl;
-            delete src;
-            exit(-1); // Use exit here cautiously; it will terminate the program. Consider other ways to handle errors.
-        }
-
-        fio_option_list opt;
-        src->get_field_options(&opt);
-        opt.set_option(FIO_TIMESLICE, 0);
-        opt.set_option(FIO_PART, FIO_TOTAL);
-
-        fio_field *mag_field;
-        result = src->get_field(FIO_MAGNETIC_FIELD, &mag_field, &opt);
-        if (result != FIO_SUCCESS) {
-            std::cout << "Error obtaining magnetic field from " << source_path << std::endl;
-            delete src; // Note: deleting src might not be enough; you might need to specifically release mag_field or other resources depending on the API.
-            exit(-1);
-        }
-
-#pragma omp for
-        for (int i = 0; i < 10; i++) {
-            x[i][0] = 0.6;
-            x[i][1] = 0.3;
-            x[i][2] = 0.1;
-            result = mag_field->eval(x[i], B[i]);
-
-            // Assuming you want to print inside the parallel region
-            // Be cautious about doing this to avoid jumbled output
-            std::stringstream ss;
-            ss << "BR, BPhi, BZ = " << B[i][0] << ", " << B[i][1] << ", " << B[i][2] << " from thread " << omp_get_thread_num() << std::endl;
-            std::cout << ss.str();
-        }
-
-        // Cleanup for each thread
-        delete src; // Again, make sure you're managing memory correctly according to the API's requirements.
+    for (int i = 0; i < 10; i++) {
+        x[i][0] = 0.6;
+        x[i][1] = 0.4;
+        x[i][2] = 0.1;
     }
 
-    // Note: Removed the final printing loop to avoid repeated printing
+    int i;
+
+    size_t hint_size = (*src).sizeof_search_hint();
+    std::cout << "Size of search hint: " << hint_size << std::endl;
+    void *hint = calloc(10, hint_size);
+
+    // #pragma omp parallel for
+    for (i = 0; i < 10; i++) {
+        void *h = ((char *)hint);
+        result = (*mag_field).eval(x[i], B[i], h);
+        h_array[i] = *((int *)h);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "\n"
+                  << h_array[i] << std::endl;
+        std::cout << "R, Phi, Z = " << x[i][0] << ", " << x[i][1] << ", " << x[i][2] << std::endl;
+        std::cout << "BR, BPhi, BZ = " << B[i][0] << ", " << B[i][1] << ", " << B[i][2] << std::endl;
+    }
 
     return 0;
 }
