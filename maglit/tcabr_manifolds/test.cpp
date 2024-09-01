1c748add83797881dd45d960f72ecf86b7fe09fb
ojrfernandes <jose.r.fernandes.j@gmail.com>
2024-08-31 21:52:24 -0300

    updated test function

--- maglit/tcabr_manifolds/test.cpp
+++ maglit/tcabr_manifolds/test.cpp
@@ -2,7 +2,9 @@
 
 #include "../maglit.h"
 #include <armadillo>
+#include <chrono>
 #include <iomanip>
+#include <thread>
 #include <utility>
 
 // Apply the map to a given point
@@ -42,8 +44,8 @@ void eval_jacobian(maglit &source, double R, double Z, double Phi, double h, dou
 // Iteratively find the closest 1 period fixed point from the initial guess
 bool x_point(maglit &source, double &R, double &Z, double &Phi, double tol, int max_iter) {
     for (int iter = 0; iter < max_iter; ++iter) {
-        std::cout << "iter: " << iter << std::endl;
-        // initialize jacobian and identity
+        // std::cout << "iter: " << iter << std::endl;
+        //  initialize jacobian and identity
         double jacobian[2][2];
         eval_jacobian(source, R, Z, Phi, 1e-5, jacobian);
         double identity[2][2] = {{1, 0}, {0, 1}};
@@ -71,8 +73,8 @@ bool x_point(maglit &source, double &R, double &Z, double &Phi, double tol, int 
 
         // compute (R1,RZ)
         std::pair<double, double> R1_Z1 = apply_map(source, R, Z, Phi);
-        double                    R1 = R1_Z1.first;
-        double                    Z1 = R1_Z1.second;
+        double R1 = R1_Z1.first;
+        double Z1 = R1_Z1.second;
 
         // update the position
         double delta_R = R1 - (jacobian[0][0] * R + jacobian[0][1] * Z);
@@ -96,32 +98,14 @@ bool x_point(maglit &source, double &R, double &Z, double &Phi, double tol, int 
     return false;
 }
 
-// test the x_point function
-int main() {
-    char   source_path[] = "/home/jfernandes/Software/maglib_local/maglit/our_m3dc1_data/i_coils/n03/C1.h5";
-    maglit tracer(source_path, FIO_M3DC1_SOURCE);
-    tracer.inverse_map(false);
-    double R_xpoint = 0.48;
-    double Z_xpoint = -0.2;
-    double Phi = 0;
-
-    bool found_xp = x_point(tracer, R_xpoint, Z_xpoint, Phi, 1e-12, 100);
-    if (!found_xp) {
-        std::cerr << "Failed to find the x-point" << std::endl;
-        return 1;
-    }
-
-    std::cout << std::fixed << std::setprecision(15);
-    std::cout << "x-point found at: " << std::endl;
-    std::cout << "R: " << R_xpoint << " Z: " << Z_xpoint << std::endl;
-    // std::cout << std::defaultfloat << std::setprecision(6);
-
+void primarySegment(maglit &source, double R_xpoint, double Z_xpoint, double Phi, double epsilon, double num_points, std::vector<double> &R_vec, std::vector<double> &Z_vec) {
     // Evaluate the jacobian at the x-point
     double jacobian[2][2];
-    eval_jacobian(tracer, R_xpoint, Z_xpoint, Phi, 1e-5, jacobian);
+    eval_jacobian(source, R_xpoint, Z_xpoint, Phi, 1e-5, jacobian);
     std::cout << "Jacobian at the x-point: " << std::endl;
-    std::cout << jacobian[0][0] << " " << jacobian[0][1] << std::endl;
-    std::cout << jacobian[1][0] << " " << jacobian[1][1] << std::endl;
+    std::cout << "[" << jacobian[0][0] << " " << jacobian[0][1] << "]" << std::endl;
+    std::cout << "[" << jacobian[1][0] << " " << jacobian[1][1] << "]\n"
+              << std::endl;
 
     // Convert the jacobian to an armadillo matrix
     arma::mat jacobian_arma = {{jacobian[0][0], jacobian[0][1]}, {jacobian[1][0], jacobian[1][1]}};
@@ -133,10 +117,13 @@ int main() {
     arma::eig_gen(eigval, eigvec, jacobian_arma);
 
     std::cout << "Eigenvalues: " << std::endl;
-    std::cout << eigval << std::endl;
+    std::cout << eigval[0].real() << " " << eigval[1].real() << "\n"
+              << std::endl;
 
     std::cout << "Eigenvectors: " << std::endl;
-    std::cout << eigvec << std::endl;
+    std::cout << "[" << eigvec.col(0)[0].real() << " " << eigvec.col(0)[1].real() << "]" << std::endl;
+    std::cout << "[" << eigvec.col(1)[0].real() << " " << eigvec.col(1)[1].real() << "]\n"
+              << std::endl;
 
     // Find the eigenvector corresponding to the eigenvalue with |lambda| > 1
     arma::cx_vec selected_eigvec;
@@ -154,47 +141,89 @@ int main() {
     double v_R = selected_eigvec[0].real();
     double v_Z = selected_eigvec[1].real();
     std::cout << "Selected eigenvector (|lambda| > 1): " << std::endl;
-    std::cout << v_R << std::endl;
-    std::cout << v_Z << std::endl;
-    std::cout << sqrt(v_R * v_R + v_Z * v_Z) << std::endl;
-
-    // Define the small distance epsilon
-    double epsilon = 1e-8;
+    std::cout << "[" << v_R << " " << v_Z << "]\n"
+              << std::endl;
 
     // Calculate the new point (R_new, Z_new)
     double R_new = R_xpoint + epsilon * v_R;
     double Z_new = Z_xpoint + epsilon * v_Z;
 
-    std::cout << "New point in the vicinity x-point: " << std::endl;
-    std::cout << "R_new: " << R_new << " Z_new: " << Z_new << std::endl;
+    std::cout << "Primary segment first point: " << std::endl;
+    std::cout << "R_new: " << R_new << " Z_new: " << Z_new << "\n"
+              << std::endl;
 
     // Apply the map M to the new point: M(R_new, Z_new) = (MR, MZ)
-    std::pair<double, double> M_rz = apply_map(tracer, R_new, Z_new, Phi);
-    double                    MR = M_rz.first;
-    double                    MZ = M_rz.second;
+    std::pair<double, double> M_rz = apply_map(source, R_new, Z_new, Phi);
+    double MR = M_rz.first;
+    double MZ = M_rz.second;
 
-    std::cout << "Mapped point: " << std::endl;
-    std::cout << "MR: " << MR << " MZ: " << MZ << std::endl;
+    std::cout << "Primary segment last point: " << std::endl;
+    std::cout << "MR: " << MR << " MZ: " << MZ << "\n"
+              << std::endl;
 
     // Interpolate points between (R_new, Z_new) and (MR, MZ)
-    int    num_points = 3000;
     double step_R = (MR - R_new) / (num_points - 1);
     double step_Z = (MZ - Z_new) / (num_points - 1);
-
-    std::vector<double> R_vec(num_points);
-    std::vector<double> Z_vec(num_points);
 
     for (int i = 0; i < num_points; ++i) {
         R_vec[i] = R_new + i * step_R;
         Z_vec[i] = Z_new + i * step_Z;
     }
+};
+
+// Print a progress bar
+void progressBar(float progress) {
+    int barWidth = 50;
+
+    std::cout << "[";
+    int pos = barWidth * progress;
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos)
+            std::cout << "=";
+        else if (i == pos)
+            std::cout << ">";
+        else
+            std::cout << " ";
+    }
+    std::cout << "] " << int(progress * 100.0) << " %\r";
+    std::cout.flush();
+}
+
+// test the x_point function
+int main() {
+    char source_path[] = "/home/jfernandes/Software/maglib_local/maglit/our_m3dc1_data/i_coils/n03/C1.h5";
+    maglit tracer(source_path, FIO_M3DC1_SOURCE);
+    tracer.inverse_map(false);
+    double R_xpoint = 0.48;
+    double Z_xpoint = -0.2;
+    double Phi = 0;
+
+    bool found_xp = x_point(tracer, R_xpoint, Z_xpoint, Phi, 1e-12, 100);
+    if (!found_xp) {
+        std::cerr << "Failed to find the x-point" << std::endl;
+        return 1;
+    }
+
+    std::cout << std::fixed << std::setprecision(16);
+    std::cout << std::endl;
+    std::cout << "x-point found at: " << std::endl;
+    std::cout << "R: " << R_xpoint << " Z: " << Z_xpoint << "\n"
+              << std::endl;
+    // std::cout << std::defaultfloat << std::setprecision(6);
+
+    // Define the small distance epsilon
+    double epsilon = 1e-7;
+    int num_points = 100;
+    std::vector<double> R_vec(num_points);
+    std::vector<double> Z_vec(num_points);
+    primarySegment(tracer, R_xpoint, Z_xpoint, Phi, epsilon, num_points, R_vec, Z_vec);
 
     // Compute manifold
-    int    crossings = 12;
+    int crossings = 9;
     double phi_max = 2 * M_PI;
     double Phi_aux = 0;
-    FILE  *f1 = fopen("crossings/crossings_icoil_n3_1_.dat", "a");
-    int    status = SODE_CONTINUE_GOOD_STEP;
+    FILE *f1 = fopen("crossings/crossings_icoil_n3_1_.dat", "a");
+    int status = SODE_CONTINUE_GOOD_STEP;
     for (int j = 0; j < num_points; j++) {
         for (int i = 0; i < crossings; i++) {
             tracer.reset();
@@ -211,9 +240,15 @@ int main() {
             }
             tracer.clear_hint();
         }
-        // print j
-        printf("j: %d\n", j);
+
+        // print progress bar
+        float progress = (float)j / num_points;
+        progressBar(progress);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
+
+    std::cout << "\nDone! \n"
+              << std::endl;
     fclose(f1);
 
     return 0;
