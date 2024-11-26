51252fc4f5b1dbfee25e70004557cf115154ca5e
ojrfernandes <jose.r.fernandes.j@gmail.com>
2024-11-26 17:07:12 -0300

    xnull and znull values are now directly read from the hdf5 file and used as initial guesses for findinf the X-point's coordinates.

--- maglit/tcabr_manifolds/source/input_read.cpp
+++ maglit/tcabr_manifolds/source/input_read.cpp
@@ -1,12 +1,11 @@
 #include "input_read.h"
 
 // Constructor
-input_read::input_read(const std::string &readingPath) : source_path(nullptr), output_path(nullptr), reading_path(readingPath) {}
+input_read::input_read(const std::string &readingPath) : source_path(nullptr), reading_path(readingPath) {}
 
 // Destructor to free allocated memory
 input_read::~input_read() {
     delete[] source_path;
-    delete[] output_path;
 }
 
 // read paths to the source, shape and output from a text file along with the initial grid parameters
@@ -31,10 +30,9 @@ bool input_read::readInputFile() {
                 break;
             case 1:
                 // Allocate memory for output_path and copy the string
-                output_path = new char[line.length() + 1];
-                std::strcpy(output_path, line.c_str());
+                output_path = line;
                 // Check if the output file already exists
-                if (access(output_path, F_OK) != -1) {
+                if (access(output_path.c_str(), F_OK) != -1) {
                     std::cerr << "There is a file with the same name saved to the chosen directory. "
                               << "Please change the output file name to avoid overwriting your data."
                               << std::endl;
@@ -51,21 +49,15 @@ bool input_read::readInputFile() {
                 Phi = std::stod(line);
                 break;
             case 5:
-                R_xPoint = std::stod(line);
-                break;
-            case 6:
-                Z_xPoint = std::stod(line);
-                break;
-            case 7:
                 epsilon = std::stod(line);
                 break;
-            case 8:
+            case 6:
                 nSeg = std::stoi(line);
                 break;
-            case 9:
+            case 7:
                 l_lim = std::stod(line);
                 break;
-            case 10:
+            case 8:
                 theta_lim = std::stod(line);
                 break;
             default:
@@ -76,11 +68,66 @@ bool input_read::readInputFile() {
         }
     }
 
-    if (line_index < 11) {
+    if (line_index < 9) {
         std::cerr << "File does not contain enough data. Expected 10 values, found " << line_index << "." << std::endl;
         return false;
     }
 
     pathFile.close();
+
+    // call readHDF5File to read xnull and znull from the HDF5 file
+    if (!readHDF5File()) {
+        std::cerr << "Error reading xnull and znull from the HDF5 file." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+// read xnull and znull from the HDF5 file
+bool input_read::readHDF5File() {
+    // paths to the xnull and znull datasets in the HDF5 file
+    const std::string xnullPath = "scalars/xnull";
+    const std::string znullPath = "scalars/znull";
+
+    try {
+        // Open the HDF5 file
+        hid_t file = H5Fopen(source_path, H5F_ACC_RDONLY, H5P_DEFAULT);
+
+        // Open the datasets
+        hid_t xnullDataset = H5Dopen(file, "scalars/xnull", H5P_DEFAULT);
+        hid_t znullDataset = H5Dopen(file, "scalars/znull", H5P_DEFAULT);
+
+        // Get the dataspace of the datasets
+        hid_t xnullSpace = H5Dget_space(xnullDataset);
+        hid_t znullSpace = H5Dget_space(znullDataset);
+
+        // Get the number of elements in the datasets
+        hsize_t xnullSize;
+        hsize_t znullSize;
+        H5Sget_simple_extent_dims(xnullSpace, &xnullSize, nullptr);
+        H5Sget_simple_extent_dims(znullSpace, &znullSize, nullptr);
+
+        // Read the data into vectors
+        std::vector<double> xnullData(xnullSize);
+        std::vector<double> znullData(znullSize);
+
+        H5Dread(xnullDataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, xnullData.data());
+        H5Dread(znullDataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, znullData.data());
+
+        // save the xnull and znull values to R_xpoint and Z_xpoint
+        R_xPoint = xnullData[0];
+        Z_xPoint = znullData[0];
+
+        // Close datasets and file
+        H5Dclose(xnullDataset);
+        H5Dclose(znullDataset);
+        H5Fclose(file);
+
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return false;
+    }
+
     return true;
 }
--- maglit/tcabr_manifolds/source/input_read.h
+++ maglit/tcabr_manifolds/source/input_read.h
@@ -1,11 +1,13 @@
 #ifndef INPUT_READ_H
 #define INPUT_READ_H
-#define INPUT_READ_V 241021 // version (yy.mm.dd)
+#define INPUT_READ_V 241126 // version (yy.mm.dd)
 
 #include <cstring>
 #include <fstream>
+#include <hdf5.h>
 #include <iostream>
 #include <unistd.h>
+#include <vector>
 
 class input_read {
   public:
@@ -15,10 +17,12 @@ class input_read {
     ~input_read();
     // read paths to the source, shape and output from a text file along with the initial grid parameters
     bool readInputFile();
+    // read xnull and znull from the HDF5 file
+    bool readHDF5File();
 
     // Variables to store the paths and parameters
     char *source_path;
-    char *output_path;
+    std::string output_path;
     int timeslice;
     int stability;
     double Phi;
